f5d57163b8a1b1dc0da52c50f530225fdec3443c
ashley-k2x <138651163+ashley-k2x@users.noreply.github.com>
2024-02-19 12:01:44 -0800

    training done on GPU

--- src/train.py
+++ src/train.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     config = read_yaml("config.yaml")
 
     category_lines, all_categories = load_data(config.data)
-    dagshub.init(
-        os.environ.get("dagshub_repo"), os.environ.get("dagshub_user"), mlflow=True
-    )
+    # dagshub.init(
+    #     os.environ.get("dagshub_repo"), os.environ.get("dagshub_user"), mlflow=True
+    # )
 
     trainer = Trainer(params, category_lines, all_categories)
     model = trainer.fit(config, params)
